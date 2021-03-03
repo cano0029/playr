765a3ae6cb7393e099db114da65c5bae37c22223
Johanna Cano <cano0029@algonquinlive.com>
2021-03-03 15:08:11 -0500

    Add media playr

--- www/js/index.js
+++ www/js/index.js
@@ -20,7 +20,26 @@
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 
+const CODES = {
+    error: {
+        1: 'MEDIA_ERR_ABORTED',
+        2: 'MEDIA_ERR_NETWORK',
+        3: 'MEDIA_ERR_DECODE',
+        4: 'MEDIA_ERR_NONE_SUPPORTED'
+
+    },
+
+    status: {
+        0: 'MEDIA_NONE',
+        1: 'MEDIA_STARTING',
+        2: 'MEDIA_RUNNING',
+        3: 'MEDIA_PAUSED',
+        4: 'MEDIA_STOPPED'
+    }
+}
+
 const APP = {
+    media: null, 
     tracks: [
         {
             id: 412,
@@ -28,7 +47,8 @@ const APP = {
             track: 'Dont Worry Be Happy',
             length: 0,
             image: './img/jasonLeung-unsplash.jpg',
-            path: './media/bob-mcferrin/dont-worry-be-happy.mp3',
+            src: 'media/bob-mcferrin/dont-worry-be-happy.mp3',
+            volume: 0.5
         },
         {
             id: 222,
@@ -36,7 +56,8 @@ const APP = {
             track: 'Come to Me',
             length: 0,
             image: './img/gooGooDolls.jpg',
-            path: './media/goo-goo-dolls/come-to-me.mp3',
+            src: 'media/goo-goo-dolls/come-to-me.mp3',
+            volume: 0.5
         },
         {
             id: 141,
@@ -44,7 +65,8 @@ const APP = {
             track: 'Walking the Wire',
             length: 0,
             image: './img/imagineDragons.jpg',
-            path: './media/imagine-dragons/walking-the-wire.mp3',
+            src: 'media/imagine-dragons/walking-the-wire.mp3',
+            volume: 0.5
         },
         {
             id: 312,
@@ -52,7 +74,8 @@ const APP = {
             track: 'Take me Home, Country Roads',
             length: 0,
             image: './img/johnDenver.jpg',
-            path: './media/john-denver/take-me-home.mp3',
+            src: 'media/john-denver/take-me-home.mp3',
+            volume: 0.5
         },
         {
             id: 566,
@@ -60,15 +83,18 @@ const APP = {
             track: 'Gorgeous',
             length: 0,
             image: './img/xAmbassadors.jpg',
-            path: './media/x-ambassadors/gorgeous.mp3',
+            src: 'media/x-ambassadors/gorgeous.mp3',
+            volume: 0.5
         },
     ],
     init: () => {
         APP.addListeners()
         APP.showPlaylist()
+        APP.mountMedia()
     },
     addListeners: () => {
         document.getElementById('btnClose').addEventListener('click', APP.displayHome)
+        document.getElementById('play').addEventListener('click', APP.play)
     },
     showPlaylist: () => {
         console.log("suhhh dude");
@@ -96,35 +122,22 @@ const APP = {
 
             df.append(li);
 
-            li.addEventListener("click", APP.displaySong)
+            li.addEventListener("click", APP.displaySongPage)
         })
         )
         list.append(df)
     },
+
     displayHome: () => {
-        document.querySelector('.page.active').classList.remove('active');
-        document.getElementById('home').classList.add('active');
+        document.querySelector('.page.active').classList.remove('active')
+        document.getElementById('home').classList.add('active')
     },
-    displaySong: (ev) => {
+    displaySongPage: (ev) => {
         console.log('hi')
-        document.querySelector('.page.active').classList.remove('active');
-        document.getElementById('track').classList.add('active');
+        document.querySelector('.page.active').classList.remove('active')
+        document.getElementById('track').classList.add('active')
 
         APP.songCard(ev)
-        // let clickedThing = ev.target;
-        // let track = clickedThing.closest('[data-key]');
-        // console.log(track);
-        
-        // if(track) {
-        //     const id = parseInt(track.getAttribute('data-key'));
-        //     APP.tracks.find(song => {
-        //         if (song.id === id) {
-        //         document.getElementById('track-image').src = song.image; 
-        //         document.getElementById('track-title').textContent = song.track;
-        //         document.getElementById('track-artist').textContent = song.artist;
-        //         }
-        //     });
-        // }
     },
 
     songCard: (ev) => {
@@ -142,7 +155,29 @@ const APP = {
                 }
             });
         }
-    }
+    },
+
+    mountMedia: () => {
+        APP.media = new Media (
+            APP.tracks.src, 
+            APP.handleMediaSuccess, 
+            APP.handleMediaError, 
+            APP.handleMediaStatusChange 
+        )
+    },
+    handleMediaSuccess: () => {
+        console.log('WOOHOO! Successfully completed the media task')
+    },
+
+    handleMediaError: (error) => {
+        console.log(CODES.error[code], CODES.error[message])
+    },
+
+    handleMediaStatusChange: () => {
+        //optional parameter
+    }, 
+
+    play: () => {APP.media.play()},
 };
 
 const ready = "cordova" in window ? "deviceready" : "DOMContentLoaded";
