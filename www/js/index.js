82a0a6a973b9bb6899a96463e62adec4add87f20
Johanna Cano <cano0029@algonquinlive.com>
2021-03-09 12:08:36 -0500

    Add more songs

--- www/js/index.js
+++ www/js/index.js
@@ -85,6 +85,69 @@ const APP = {
             src: './media/gorgeous.mp3',
             volume: 0.5
         },
+        {
+            id: 5,
+            artist: 'Don McLean',
+            track: 'American Pie',
+            length: 0,
+            image: './img/donMcLean.png',
+            src: './media/american-pie.mp3',
+            volume: 0.5
+        },
+        {
+            id: 6,
+            artist: 'Van Morrison',
+            track: 'Brown Eyed Girl',
+            length: 0,
+            image: './img/vanMorrison.jpg',
+            src: './media/brown-eyed-girl.mp3',
+            volume: 0.5
+        },
+        {
+            id: 7,
+            artist: 'Blue Swede',
+            track: 'Hooked on a Feeling',
+            length: 0,
+            image: './img/blueSwede.jpg',
+            src: './media/hooked-on-a-feeling.mp3',
+            volume: 0.5
+        },
+        {
+            id: 8,
+            artist: 'Miguel',
+            track: 'Pineapple Skies',
+            length: 0,
+            image: './img/miguel.jpg',
+            src: './media/pineapple-skies.mp3',
+            volume: 0.5
+        },
+        {
+            id: 9,
+            artist: 'Matroda',
+            track: 'Forget It',
+            length: 0,
+            image: './img/matroda.jpg',
+            src: './media/forget-it.mp3',
+            volume: 0.5
+        },
+        {
+            id: 10,
+            artist: 'MGMT',
+            track: 'Electric Feel',
+            length: 0,
+            image: './img/mgmt.jpg',
+            src: './media/electric-feel.mp3',
+            volume: 0.5
+        },
+        {
+            id: 11,
+            artist: 'Paul Simon',
+            track: 'You Can Call Me Al',
+            length: 0,
+            image: './img/paulSimon.jpg',
+            src: './media/you-can-call-me-al.mp3',
+            volume: 0.5
+        },
 
         // TO DO:add more songs
     ],
